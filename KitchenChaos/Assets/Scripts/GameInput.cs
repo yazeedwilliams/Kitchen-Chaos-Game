5174047ec0adf892499e4b3556786baae0d121e2
yazeed-williams <yazeedwilliams13@gmail.com>
2024-01-12 17:38:02 +0200

    New input actions logic

--- KitchenChaos/Assets/Scripts/GameInput.cs
+++ KitchenChaos/Assets/Scripts/GameInput.cs
@@ -4,29 +4,17 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    private PlayerInputActions playerInputActions;
+
+    private void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
     public Vector2 GetMovementVectorNormalized()
     {
-        Vector2 inputVector = new(0, 0);
-
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputVector.y = +1;
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            inputVector.x -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            inputVector.y -= 1;
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputVector.x += 1;
-        }
+        // accessing the input actions from the player input actions
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
 
         // keeps the movement speed the same when pressing two directions at once (diagonal movement) as when pressing one direction
         inputVector = inputVector.normalized;
