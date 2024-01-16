63be28670cded1f6ce09e965328b030f5ff383e9
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-01-16 11:21:39 +0200

    Spawn objects on counter

--- KitchenChaos/Assets/Scripts/ClearCounter.cs
+++ KitchenChaos/Assets/Scripts/ClearCounter.cs
@@ -2,8 +2,12 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour
 {
+    [SerializeField] private Transform tomatoPrefab;
+    [SerializeField] private Transform counterTopPoint;
     public void Interact()
     {
         Debug.Log("Interact");
+        Transform tomatoTransform = Instantiate(tomatoPrefab, counterTopPoint);
+        tomatoTransform.localPosition = Vector3.zero;
     }
 }
