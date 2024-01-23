963bf772c0293f17362564aef6772d0ae27603f7
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-01-23 12:01:36 +0200

    Created counter script

--- KitchenChaos/Assets/Scripts/Counters/StoveCounter.cs
+++ KitchenChaos/Assets/Scripts/Counters/StoveCounter.cs
@@ -1,8 +1,72 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using static CuttingCounter;
 
 public class StoveCounter : BaseCounter
 {
     [SerializeField] private FryingRecipeSO[] fryingRecipeSOArray;
+
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            // There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                {
+                    // player carrying something that can be fried
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+                }
+            }
+            else
+            {
+                // Player not carrying anything
+            }
+        }
+        else
+        {
+            // There is a KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is caryying something
+            }
+            else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        return fryingRecipeSO != null;
+    }
+
+    private KitchenObjectSO GetOutPutForInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        FryingRecipeSO fryingRecipeSO = GetFryingRecipeSOWithInput(inputKitchenObjectSO);
+        if (fryingRecipeSO != null)
+        {
+            return fryingRecipeSO.output;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    private FryingRecipeSO GetFryingRecipeSOWithInput(KitchenObjectSO inputKitchenObjectSO)
+    {
+        foreach (FryingRecipeSO fryingRecipeSO in fryingRecipeSOArray)
+        {
+            if (fryingRecipeSO.input == inputKitchenObjectSO)
+            {
+                return fryingRecipeSO;
+            }
+        }
+        return null;
+    }
 }
