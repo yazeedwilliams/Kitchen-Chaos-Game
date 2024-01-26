5658f8bd59358c203cf79d11124a9a3dd07d1cc6
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-01-26 15:46:02 +0200

    Plate pickup

--- KitchenChaos/Assets/Scripts/Counters/StoveCounter.cs
+++ KitchenChaos/Assets/Scripts/Counters/StoveCounter.cs
@@ -121,6 +121,21 @@ public class StoveCounter : BaseCounter, IHasProgress
             if (player.HasKitchenObject())
             {
                 // Player is caryying something
+                // Player is caryying something
+                if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject))
+                {
+                    // Player is holding a plate
+                    if (plateKitchenObject.TryAddIngredient(GetKitchenObject().GetKitchenObjectSO()))
+                    {
+                        GetKitchenObject().DestroySelf();
+
+                        state = State.Idle;
+
+                        OnStateChanged?.Invoke(this, new OnStateChangedEventArgs { state = state });
+
+                        OnProgressChanged?.Invoke(this, new IHasProgress.OnProgressChangedEventArgs { progressNormalized = 0f });
+                    }
+                }
             }
             else
             {
--- KitchenChaos/Assets/Scripts/KitchenObject.cs
+++ KitchenChaos/Assets/Scripts/KitchenObject.cs
@@ -43,6 +43,20 @@ public class KitchenObject : MonoBehaviour
         Destroy(gameObject);
     }
 
+    public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
+    {
+        if (this is PlateKitchenObject)
+        {
+            plateKitchenObject = this as PlateKitchenObject;
+            return true;
+        }
+        else
+        {
+            plateKitchenObject = null;
+            return false;
+        }
+    }
+
     public static KitchenObject SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
     {
         // if player is not carrying anything
