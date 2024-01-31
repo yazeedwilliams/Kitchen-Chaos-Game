449365b9e714916b34f245c50c822b57f602753f
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-01-31 11:52:52 +0200

    Orders and timer for orders

--- KitchenChaos/Assets/Scripts/DeliveryManager.cs
+++ KitchenChaos/Assets/Scripts/DeliveryManager.cs
@@ -5,5 +5,30 @@ using UnityEngine;
 public class DeliveryManager : MonoBehaviour
 {
     [SerializeField] private RecipeListSO recipeListSO;
+
     private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f)
+        {
+            spawnRecipeTimer = spawRecipeTimerMax;
+
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
 }
