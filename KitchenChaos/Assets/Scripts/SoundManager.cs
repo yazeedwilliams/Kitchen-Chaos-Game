d89050e4efda9eca085dc08d6c79cf965e0184f5
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-02-02 09:46:47 +0200

    Update game scene

--- KitchenChaos/Assets/Scripts/SoundManager.cs
+++ KitchenChaos/Assets/Scripts/SoundManager.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance { get; private set; }
+
     [SerializeField] private AudioClipRefsSO audioClipRefsSO;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
@@ -59,5 +66,10 @@ public class SoundManager : MonoBehaviour
     private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
     {
         AudioSource.PlayClipAtPoint(audioClip, position, volume);
+    }
+
+    public void PlayFootstepsSound(Vector3 position, float volume)
+    {
+        PlaySound(audioClipRefsSO.footstep, position, volume);
     }
 }
