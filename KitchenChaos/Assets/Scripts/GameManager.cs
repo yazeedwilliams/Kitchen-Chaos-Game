dcca038f25469a7bf72935bd6426ae71ec38ec01
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-02-05 15:33:01 +0200

    Pause functionality

--- KitchenChaos/Assets/Scripts/GameManager.cs
+++ KitchenChaos/Assets/Scripts/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManger : MonoBehaviour
     public static GameManger Instance { get; private set; }
 
     public event EventHandler OnStateChanged;
+    public event EventHandler OnGamePaused;
+    public event EventHandler OnGameUnpaused;
+
     private enum State
     {
         WaitingToStart,
@@ -21,11 +24,22 @@ public class GameManger : MonoBehaviour
     private float countdownToStartTimer = 3f;
     private float gamePlayingTimer;
     private float gamePlayingTimerMax = 10f;
+    private bool isGamePaused = false;
 
     private void Awake()
     {
         Instance = this;
         state = State.WaitingToStart;
+    }
+
+    private void Start()
+    {
+        GameInput.Instance.OnPauseAction += GameInput_OnPauseAction;
+    }
+
+    private void GameInput_OnPauseAction(object sender, EventArgs e)
+    {
+        TogglePauseGame();
     }
 
     private void Update()
@@ -87,5 +101,22 @@ public class GameManger : MonoBehaviour
     public float GetGamePlayingTimerNormalized()
     {
         return 1 - (gamePlayingTimer / gamePlayingTimerMax);
+    }
+
+    public void TogglePauseGame()
+    {
+        isGamePaused = !isGamePaused;
+        if (isGamePaused)
+        {
+            Time.timeScale = 0f;
+            OnGamePaused?.Invoke(this, EventArgs.Empty);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            OnGameUnpaused?.Invoke(this, EventArgs.Empty);
+        }
+              
+        
     }
 }
